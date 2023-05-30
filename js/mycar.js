48477f50e357567c05ccbac4a8393f76e88cf280
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-05-30 21:19:28 +0200

    created an object in js

---
+++ js/mycar.js
@@ -0,0 +1,16 @@
+const car = {
+  brand: "Tesla",
+  model: "Model 3",
+  year: 2022,
+  color: "white",
+  milage: 80000,
+  isElectric: true,
+  on: false,
+  gearbox: ["Neutral", "First", "Second", "Third"],
+  startEngine: function () {
+    console.log("Engine started");
+  },
+  stopEngine: function () {
+    console.log("Engine stopped");
+  },
+};
