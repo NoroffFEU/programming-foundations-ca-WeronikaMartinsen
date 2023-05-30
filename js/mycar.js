9d85b241d2b9612cb562bd7c5da1ffb53a21bf78
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-05-30 21:29:11 +0200

    working on readme

--- js/mycar.js
+++ js/mycar.js
@@ -3,10 +3,10 @@ const car = {
   model: "Model 3",
   year: 2022,
   color: "white",
-  milage: 80000,
+  milage: 10000,
   isElectric: true,
   on: false,
-  gearbox: ["Neutral", "First", "Second", "Third"],
+  features: ["Autopilot", "Full self-driving", "Premium interior"],
   startEngine: function () {
     console.log("Engine started");
   },
