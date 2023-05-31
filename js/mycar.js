d4be46e256da485081024103316af7f42cd35426
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2023-05-31 10:43:09 +0200

    practising js

--- js/mycar.js
+++ js/mycar.js
@@ -14,3 +14,27 @@ const car = {
     console.log("Engine stopped");
   },
 };
+
+//Function
+
+//Statment practising
+
+var getCar = "Tesla";
+var year = 2023;
+var isElectric = true;
+
+if (getCar === "Tesla" && year >= 2022 && isElectric === true) {
+  console.log("Car is accepted");
+} else {
+  if (getCar !== "Tesla") {
+    console.log("Car must be a Tesla.");
+  }
+  if (year < 2020) {
+    console.log("The car must to from year 2022 or more.");
+  }
+  if (isElectric !== true) {
+    console.log("The car must be electric");
+  }
+}
+
+//Arrays practising
